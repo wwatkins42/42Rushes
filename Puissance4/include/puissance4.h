db69054bc51da2736eaa0f0006977c8646b477e2
William WATKINS <wwatkins@e1r7p4.42.fr>
2016-02-27 09:11:43 +0100

    Rush started

---
+++ Puissance4/include/puissance4.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   puissance4.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/27 09:06:49 by wwatkins          #+#    #+#             */
+/*   Updated: 2016/02/27 09:09:10 by wwatkins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUISSANCE_4_H
+# define PUISSANCE_4_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <libft.h>
+# include <time.h>
+
+#endif
